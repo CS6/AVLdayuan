3c49cca7fa5b02ba097d19c7f6de54fb89829106
CS6 <sduum82@gmail.com>
2019-12-22 18:03:56 +0800

    WIP QAQ 待補後台-U

--- src/App.js
+++ src/App.js
@@ -5,48 +5,53 @@ import RT from './RR';
 // import Main from './components/Main.js'
 // import Book from './components/Main.js'
 import BookToDoList from './components/BookToDoList'
-import { Button,List, WhiteSpace, WingBlank } from 'antd-mobile';
+import { Button, List, WhiteSpace, WingBlank } from 'antd-mobile';
 import 'antd-mobile/dist/antd-mobile.css';  // or 'antd-mobile/dist/antd-mobile.less'
 // import Counter from './examples/app';
 import Counter from './examples/app';
 import StoreTab from './components/store/StoreTab'
+import Login from './components/Admin/Login'
+import StoreSlect from './components/store/StoreSlect'
+
 
 function App() {
   return (
     <div className="App">
       <div>
-      <StoreTab/>
+      <Login />
+
+        <StoreSlect />
+        <StoreTab />
 
       </div>
-      <BookToDoList/>
+      {/* <BookToDoList />
       <Counter />
-  
+
 
       <div style={{
-		// backgroundColor: '#FAF',
-		borderRadius:22,
-		borderColor:'#234567',
-		outlineColor:'#FF4567',
-		borderBlockColor:'#234567',
-		color :'#000',
-    height:20,
-    borderWidth: 5,
-    border:'5px',
-    borderBottom: "3px solid rgb(212, 212, 212)",
-    borderTop: "3px solid #234567",
+        // backgroundColor: '#FAF',
+        borderRadius: 22,
+        borderColor: '#234567',
+        outlineColor: '#FF4567',
+        borderBlockColor: '#234567',
+        color: '#000',
+        height: 20,
+        borderWidth: 5,
+        border: '5px',
+        borderBottom: "3px solid rgb(212, 212, 212)",
+        borderTop: "3px solid #234567",
 
-    fontSize: 16,
-        width:100,
-    borderBlockEndColor:"#454542",
-	}}>s</div>
-      <RT/>
-      
+        fontSize: 16,
+        width: 100,
+        borderBlockEndColor: "#454542",
+      }}>s</div>
+      <RT />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -56,31 +61,29 @@ function App() {
           Learn React
         </a>
       </header>
-           
-
       <Button>default</Button><WhiteSpace />
-    <Button disabled>default disabled</Button><WhiteSpace />
+      <Button disabled>default disabled</Button><WhiteSpace />
 
-  <List style={{ margin: '5px 0', backgroundColor: 'white' }}>
-    <List.Item
-      extra={<Button type="ghost" size="small" inline>small</Button>}
-      multipleLine
-    >
-      Regional manager
+      <List style={{ margin: '5px 0', backgroundColor: 'white' }}>
+        <List.Item
+          extra={<Button type="ghost" size="small" inline>small</Button>}
+          multipleLine
+        >
+          Regional manager
       <List.Item.Brief>
-        Can be collected, refund, discount management, view data and other operations
+            Can be collected, refund, discount management, view data and other operations
       </List.Item.Brief>
-    </List.Item>
-    <List.Item
-      extra={<Button type="primary" size="small" inline>small</Button>}
-      multipleLine
-    >
-      Regional manager
+        </List.Item>
+        <List.Item
+          extra={<Button type="primary" size="small" inline>small</Button>}
+          multipleLine
+        >
+          Regional manager
       <List.Item.Brief>
-        Can be collected, refund, discount management, view data and other operations
+            Can be collected, refund, discount management, view data and other operations
       </List.Item.Brief>
-    </List.Item>
-  </List>
+        </List.Item>
+      </List> */}
 
     </div>
   );
--- src/components/store/StoreTab.js
+++ src/components/store/StoreTab.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-// import firebase from 'firebase';
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
 import storeDB from "./data";
 import GetTime from "./GetTime";
 
 import 'antd/dist/antd.css';
 import { Button, List, WhiteSpace, WingBlank } from 'antd-mobile';
-
-import { Table, Divider, Tag } from 'antd';
-
+import { Table, Divider, Tag } from 'antd'
 class StoreTab extends Component {
   constructor(props) { //若要使用這個方法
     super(props) //必須先執行super(props)，若有用到props的話會找不到this.props，沒用到props也可以只寫super()
@@ -22,7 +17,7 @@ class StoreTab extends Component {
         { lat: 85.955413, lng: 50.337844 },
         { lat: 95.955413, lng: 45.337844 },
       ],
-      completed:false,
+      completed: false,
       activeStoreList: [],
 
     }
@@ -52,7 +47,7 @@ class StoreTab extends Component {
     var d = new Date()
     // if (    storeDB[0].Mon[0] < d.getHours().toString() + ":" + d.getMinutes().toString() && d.getHours().toString() + ":" + d.getMinutes().toString() < storeDB[0].Mon[1]    ){
     // }
-    let activeStore = storeDB.filter(index => index.Mon[0] < d.getHours().toString() + ":" + d.getMinutes().toString() && d.getHours().toString() + ":" + d.getMinutes().toString() < index.Mon[1]);
+    let activeStore = storeDB.filter(index => index[0][0] < d.getHours().toString() + ":" + d.getMinutes().toString() && d.getHours().toString() + ":" + d.getMinutes().toString() < index[0][1]);
 
     this.setState({
       activeStoreList: activeStore
@@ -69,33 +64,35 @@ class StoreTab extends Component {
         // position: 'relative',
         boxShadow: '-1px 1px 2px rgba(0,0,0,.2)',
       }}>
-        <a>Invite {storeDB[0].store}</a>
-        <GetTime/>
+        <h1>  現在有開的店家</h1>
+
+        {/* <a>Invite {storeDB[0].store}</a> */}
+        {/* <GetTime/> */}
         {this.state.activeStoreList.map((index) =>
           // <a> {index.store}</a>
           <List.Item
-          style={{
-            textDecoration: this.state.completed  ? 'line-through' : 'none'
-          }}
-          extra={
-            <Button  activeStyle={{ backgroundColor: 'red' }} type={this.state.completed ? 'ghost' : 'primary'} size="small" inline>small</Button>
-          }
-          multipleLine
-        >
-          {console.log("text", index.store)}
-          {index.store}
-          <h1>{index.store}</h1>
-          <List.Item.Brief >
+            style={{
+              textDecoration: this.state.completed ? 'line-through' : 'none'
+            }}
+            extra={
+              <Button activeStyle={{ backgroundColor: 'red' }} type={this.state.completed ? 'ghost' : 'primary'} size="small" inline>small</Button>
+            }
+            multipleLine
+          >
+            {console.log("text", index.store)}
             {index.store}
-            Can be collected, refund, discount management, view data and other operations
+            <h1>{index.store}</h1>
+            <List.Item.Brief >
+              {index.store}
+              Can be collected, refund, discount management, view data and other operations
     </List.Item.Brief>
-    
-        </List.Item>
-          )}
+
+          </List.Item>
+        )}
         {/* {this.props.array.map((index) =>
             <AnyReactComponent lat={index.lat} lng={index.lng} text={'Kreyser Avrora'} /> // eslint-disable-line react/no-array-index-key
           )} */}
-       
+
       </div>
     );
   }
